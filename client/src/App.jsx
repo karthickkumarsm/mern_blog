cbdb6548d86cb29b90ec453d3107676943299eb3
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-05-04 10:47:32 +0530

    created header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/about';
+import About from './pages/About';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/about' element={<About/>}/>
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Navbar, TextInput, Button } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <Navbar className='border-b-2'>
+      <Link
+        to='/'
+        className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Karthick's
+        </span>
+        Blog
+      </Link>
+      <div className='flex items-center ml-auto'> {/* Use ml-auto to push the search to the right */}
+        <TextInput
+          type='text'
+          placeholder='Search...'
+          className='hidden lg:inline-block mr-2' // Show input on larger screens
+        />
+        <Button color='gray' pill> {/* Button for search, styled as pill */}
+          <AiOutlineSearch className='w-6 h-6'/>
+        </Button>
+      </div>
+    </Navbar>
+  );
+};
+
+export default Header;
