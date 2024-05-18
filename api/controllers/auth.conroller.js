e0cea0f4758bd9158bd4056b00d0e957bfb51038
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-05-18 23:51:06 +0530

    created middleware

--- api/controllers/auth.conroller.js
+++ api/controllers/auth.conroller.js
@@ -4,7 +4,7 @@ import bcryptjs from 'bcryptjs';
 export const signup = async(req,res,next) =>{
     const {username,email,password} = req.body;
     if(!username||!email||!password||username===''||email===''||password===''){
-        return res.status(400).json({message:'All fields are required'});
+       next(errorHandler(400,'All fields are required'));
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10);
