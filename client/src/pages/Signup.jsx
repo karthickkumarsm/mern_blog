7b504f4311f035e43b7c7dc9096b6fc71f817e5b
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-05-19 00:07:53 +0530

    creating signup page UI

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto'>
+        {/**left*/}
+        <div className="">
+        <Link
+        to='/'
+        className='font-bold dark:text-white text-4xl'
+      >
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+          Karthick's
+        </span>
+        Blog
+      </Link>
+      <p className='text-sm mt-5'>
+        This is Blog website. Feel free to sign up with your email and password or with your google account.
+      </p>
+        </div>
+        {/**right*/}
+        <div className="">
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
