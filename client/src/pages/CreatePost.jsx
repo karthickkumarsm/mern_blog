ab99078c9608aed5d52efde140047c953597d02c
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-06-26 22:59:48 +0530

    added create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,17 @@
+import { TextInput } from 'flowbite-react'
 import React from 'react'
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className='p-3 max-w-3xl mx-auto min-h-screen'>
+      <h1  className='text-center text-3xl my-7 font-semibold'> Create a post</h1>
+      <form className='flex flex-col gap-4'>
+        <div className="flex flex-col gap-4 sm:flex-row justify-between">
+          <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
+
+        </div>
+      </form>
+    </div>
   )
 }
 
