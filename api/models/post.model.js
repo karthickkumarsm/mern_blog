ed87e42e7380223716da001ce11f051aa8e52deb
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-06-27 21:16:36 +0530

    created create post API

---
+++ api/models/post.model.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    userId:{
+        type: String,
+        required: true,
+    },
+    const: {
+        type: String,
+        required: true,
+    },
+    title: {
+        type:String,
+        required:true,
+        unique:true,
+    },
+    image:{
+        type: String,
+        default:'https://th.bing.com/th/id/OIP.a2fq2iecNWRLsNY2x51vXgAAAA?rs=1&pid=ImgDetMain',
+    },
+    category: {
+        type: String,
+        default: 'Uncategorized',
+    },
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+},{timestamps:true}
+);
+
+const Post = mongoose.model('Post',postSchema);
+export default Post;
