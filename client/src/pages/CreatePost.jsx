3de6bd136d06562bb064d12a3bcc12b5750229ed
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-06-28 22:53:29 +0530

    completed upload image in post functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -51,8 +51,8 @@ const CreatePost = () => {
       <h1  className='text-center text-3xl my-7 font-semibold'> Create a post</h1>
       <form className='flex flex-col gap-4'>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
-          <TextInput type='text' placeholder='Title' required id='title' className='flex-1'/>
-          <Select>
+          <TextInput type='text' placeholder='Title' required id='title' className='flex-1' onChange={(e)=> setFormData({...formData,title : e.target.value})}/>
+          <Select onChange={(e)=>{setFormData({...formData,title : e.target.value})}}>
             <option value="uncategorized">Select a category</option>
             <option value="programming">Programming</option>
             <option value="lifestylehealth">Lifestyle & Health</option>
@@ -83,7 +83,7 @@ const CreatePost = () => {
             />
           )
         }
-        <ReactQuill theme="snow" placeholder='Write your content here' className='h-72 mb-12' required/>
+        <ReactQuill theme="snow" placeholder='Write your content here' className='h-72 mb-12' required onChange={(value)=>{setFormData({...formData,content:value})}}/>
         <Button type='submit' gradientDuoTone='purpleToPink' > Publish</Button>
       </form>
     </div>
