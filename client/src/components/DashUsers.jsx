f231872e92e7ed4cc1209cc3f8c8632527920a63
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-07-12 23:25:59 +0530

    added delete user functionality to admin users

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -52,18 +52,18 @@ export default function DashUsers() {
     setShowModal(false);
     try {
       const res = await fetch(
-        `/api/user/deleteuser/${userIdToDelete}/${currentUser._id}`,
+        `/api/user/delete/${userIdToDelete}`,
         {
           method: 'DELETE',
         }
       );
       const data = await res.json();
-      if (!res.ok) {
-        console.log(data.message);
-      } else {
+      if (res.ok) {
         setUsers((prev) =>
-          prev.filter((post) => post._id !== userIdToDelete)
-        );
+            prev.filter((user) => user._id !== userIdToDelete));
+            setShowModal(false);
+      } else {
+        console.log(data.message);
       }
     } catch (error) {
       console.log(error.message);
