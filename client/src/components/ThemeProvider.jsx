1da846779eed16b69b65275e1e474ac7ea9d198a
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-07-23 23:06:42 +0530

    added recent posts functionality

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-
 export const ThemeProvider = ({children}) => {
     const {theme} = useSelector(state => state.theme)
   return (
