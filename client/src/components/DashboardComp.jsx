6c921aa1ade35e4015b911c21520ff2cc8530ce1
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-08-15 22:22:43 +0530

    added dashboard functionality

--- client/src/components/DashboardComp.jsx
+++ client/src/components/DashboardComp.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react'
 import { useState } from 'react';
 import {useSelector} from 'react-redux';
-import {HiArrowNarrowUp, HiOutlineUserGroup,HiAnnotation, HiDocumentText} from 'react-icons/hi'
+import {HiArrowNarrowUp, HiOutlineUserGroup,HiAnnotation, HiDocumentText} from 'react-icons/hi';
+import {Button, Table} from 'flowbite-react';
+import {Link} from 'react-router-dom';
 
 const DashboardComp = () => {
     const [users, setUsers] = useState([]);
@@ -124,6 +126,38 @@ const DashboardComp = () => {
                     <div className="text-gray-500">Last month</div>
                 </div>
         </div>  
+       </div>
+       <div className="">
+        <div className="flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800">
+            <div className="flex justify-between p-3 text-sm font-semibold">
+                <h1 className='text-center p-2'>Recent users</h1>
+                <Button outline gradientDuoTone="purpleToPink"><Link to={"/dashboard?tab=users"}>
+                See all
+                </Link></Button>
+            </div>
+            <Table hoverable>
+                <Table.Head>
+                    <Table.HeadCell>User image</Table.HeadCell>
+                    <Table.HeadCell>Username</Table.HeadCell>
+                </Table.Head>
+                {users && users.map((user)=>{
+                    <Table.Body key={user._id} className='divide-y'>
+                        <Table.Row className='bg-white dark:border-gray-700 dark:bg-gray-800'>
+                        <Table.Cell>
+                            <img
+                            src={user.profilePicture}
+                            alt='user image'
+                            className='w-10 h-10 rounded-full bg-gray-500'
+                            />
+                        </Table.Cell>
+                        <Table.Cell>
+                            {user.username}
+                        </Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                })}
+            </Table>
+        </div>
        </div>
     </div>
   )
