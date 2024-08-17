4fd2f7c89f8a484bef35f81b61918e7aec3c4677
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-08-17 17:45:59 +0530

    implementing search functionality

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import { TextInput } from 'flowbite-react'
+import { Select, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { useLocation } from 'react-router-dom';
 
@@ -49,21 +49,62 @@ const Search = () => {
     }, [location.search]);
 
     const handleChange = (e) => {
+        if(e.target.id === 'searchTerm'){
+            setSidebarData({
+                ...sidebarData,
+                searchTerm: e.target.value
+            })
+    }
+        if(e.target.id === 'sort'){
+            const order = e.target.value || 'desc';
+            setSidebarData({
+                ...sidebarData,
+                sort:order
+            })
+        }
+        if(e.target.value === 'category'){
+            const category = e.target.value || 'uncategorized';
+            setSidebarData({
+                ...sidebarData,
+                category
+            })
+        }
+       
+    }
+
+    const handleSubmit = (e) => {
         
     }
     
   return (
-    <div>
-        <div className="">
-            <form>
-                <div className="">
-                    <label>Search Term:</label>
+    <div className='flex flex-col md:flex-row'>
+        <div className="p-7 border-b md:border-r md:min-h-screen border-gray-500">
+            <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
+                <div className="flex items-center gap-2">
+                    <label className='whitespace-nowrap font-semibold'>Search Term:</label>
                     <TextInput placeholder='Search'
                     id='searchTerm'
                     type='text'
                     value={sidebarData.searchTerm}
                     onChange={handleChange}
                     />
+                </div>
+                <div className="flex items-center gap-2">
+                    <label className='font-semibold'>Sort:</label>
+                    <Select onChange={handleChange} value={sidebarData.sort} id='sort'>
+                        <option value='desc'>Latest</option>
+                        <option value='asc'>Oldest</option>
+                    </Select>
+                </div>
+                <div className="flex items-center gap-2">
+                    <label className='font-semibold'>Category:</label>
+                    <Select onChange={handleChange} value={sidebarData.category} id='category'>
+                    <option value="uncategorized">Uncategorized</option>
+                    <option value="dsa">DSA</option>
+                    <option value="flutter">Flutter</option>
+                    <option value="mern">MERN</option>
+                    <option value="nextjs">Next js</option>
+                    </Select>
                 </div>
             </form>
         </div>
